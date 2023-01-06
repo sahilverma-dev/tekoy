7172fd0296eba05829c0001ceb64280033b8c067
Sahil Verma <domainsahil007@gmail.com>
2023-01-06 10:15:51 +0530

    database configure

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,27 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
+import "colors";
+// importing routes
+import { userRoute } from "./routes/userRoute";
+import { connectDB } from "./config/db";
 
+// configuration
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+app.use(json());
+
+// connecting database
+connectDB();
+
+// using routes
 app.get("/", (req, res) => {
   res.send("listening");
 });
+app.use("/api/v1/user", userRoute);
 
 console.clear();
 
