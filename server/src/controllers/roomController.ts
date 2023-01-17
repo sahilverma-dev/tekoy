a69d4a624f82e062d11138bec5fdc4a7e209dbaf
Sahil Verma <domainsahil007@gmail.com>
2023-01-17 10:15:46 +0530

    validation added

--- server/src/controllers/roomController.ts
+++ server/src/controllers/roomController.ts
@@ -1,65 +1,78 @@
-import { Request, Response } from "express"
-import { Room } from "../models/RoomModel"
-import { authenticateuser } from "../middleware/auth"
-import axios from "axios"
-
+import { Request, Response } from "express";
+import { Room } from "../models/RoomModel";
 
 export const createRoom = async (req: Request, res: Response) => {
-  const { roomTitle, roomThumbnail, roomCategory, userId } = req.body
-  const room = await Room.create({title: roomTitle, thumbnail: roomThumbnail, category: roomCategory, user: userId})
-  if (room) {
-    res.status(200).json({room})
+  const { title, thumbnail, userId } = req.body;
+  if (!title || !thumbnail || !userId) {
+    res.status(400).json({
+      message: "Enter all felids",
+    });
   } else {
-    res.status(400).json({message: 'Something went wrong'})
+    const room = await Room.create({
+      title,
+      thumbnail,
+      user: userId,
+    });
+    if (room) {
+      res.status(200).json({ room });
+    } else {
+      res.status(400).json({ message: "Something went wrong" });
+    }
   }
-}
+};
 
 export const getRoom = async (req: Request, res: Response) => {
-  const { roomId } = req.params
-  const room = await Room.findById(roomId)
+  const { roomId } = req.params;
+  const room = await Room.findById(roomId);
   if (room) {
-    res.status(200).json({room})
+    res.status(200).json({ room });
   } else {
-    res.status(400).json({message: 'Something went wrong'})
+    res.status(400).json({ message: "Something went wrong" });
   }
-}
+};
 
 export const getRooms = async (req: Request, res: Response) => {
-  const rooms = await Room.find()
+  const rooms = await Room.find();
   if (rooms) {
-    res.status(200).json({rooms})
+    res.status(200).json({ rooms });
   } else {
-    res.status(400).json({message: 'Something went wrong'})
+    res.status(400).json({ message: "Something went wrong" });
   }
-}
+};
 
 export const updateRoom = async (req: Request, res: Response) => {
-  const { roomId, roomTitle, roomThumbnail, roomCategory, userId } = req.body
-  const room = await Room.findById(roomId)
-  if(room){
-    if(room.user.toString() === userId){ // check if user is owner of room
-      const newRoom = await Room.findByIdAndUpdate(roomId, {title: roomTitle, thumbnail: roomThumbnail, category: roomCategory}, {new: true})
-      res.status(200).json({room: newRoom})
-    }else{
-      res.status(400).json({message: 'You are not the owner of the room'})
+  const { roomId, roomTitle, roomThumbnail, roomCategory, userId } = req.body;
+  const room = await Room.findById(roomId);
+  if (room) {
+    if (room.user.toString() === userId) {
+      // check if user is owner of room
+      const newRoom = await Room.findByIdAndUpdate(
+        roomId,
+        { title: roomTitle, thumbnail: roomThumbnail, category: roomCategory },
+        { new: true }
+      );
+      res.status(200).json({ room: newRoom });
+    } else {
+      res.status(400).json({ message: "You are not the owner of the room" });
     }
-  }else{
-    res.status(400).json({message: 'Something went wrong'})
+  } else {
+    res.status(400).json({ message: "Something went wrong" });
   }
-}
+};
 
 export const deleteRoom = async (req: Request, res: Response) => {
-  const { roomId, userId } = req.body
-  const room = await Room.findById(roomId)
+  const { roomId, userId } = req.body;
+  const room = await Room.findById(roomId);
 
-  if(room){
-    if(room.user.toString() === userId){ // check if user is owner of room
-      await Room.findByIdAndDelete(roomId)
-      res.status(200).json({message: 'Room deleted'})
-    }else{
-      res.status(400).json({message: 'You are not the owner of the room'})
+  if (room) {
+    if (room.user.toString() === userId) {
+      // check if user is owner of room
+      await Room.findByIdAndDelete(roomId);
+      res.status(200).json({ message: "Room deleted" });
+    } else {
+      res.status(400).json({ message: "You are not the owner of the room" });
     }
-  }else{
-    res.status(400).json({message: 'Something went wrong'})
+  } else {
+    res.status(400).json({ message: "Something went wrong" });
   }
-}
+};
--- server/src/models/RoomModel.ts
+++ server/src/models/RoomModel.ts
@@ -1,27 +1,31 @@
-import { Document, model, Schema } from "mongoose"
+import { Document, model, Schema } from "mongoose";
 
 interface Room {
   title: string;
   thumbnail: string;
   category: string;
   user: object;
-  created_at: Date
+  createdAt: Date;
 }
 
-const roomSchema = new Schema(
-  {
-    title: String,
-    thumbnail: String,
-    category: String,
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    created_at: {
-        type: Date,
-        default: Date.now
-    }
-  }
-)
+const roomSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-export const Room = model<Room & Document>("Room", roomSchema)
+export const Room = model<Room & Document>("Room", roomSchema);
