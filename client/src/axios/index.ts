a8b4291655568febb71abd3b5d16a56be0fb5e57
Sahil Verma <domainsahil007@gmail.com>
2023-02-14 08:08:15 +0530

    api changed

--- client/src/axios/index.ts
+++ client/src/axios/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+  // baseURL: "http://localhost:5000/api/v1",
 });
